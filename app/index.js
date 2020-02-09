1622c07f2badba0eef39b1c13a2bdd27150419e9
hassan elhamaky <hassan.elhamaky@yahoo.com>
2020-02-09 22:48:39 +0200

    initial commit

---
+++ app/index.js
@@ -0,0 +1,43 @@
+/*
+*Primary file for the API
+*
+*/
+
+//Dependencies
+var http = require ('http');
+var url = require('url');
+
+//The server should respond to all requests with a string
+var server = http.createServer(function(req,res){
+
+    //Get the URL and parse it
+    var parsedUrl = url.parse(req.url,true);
+
+    //Get the path
+    var path = parsedUrl.pathname;
+    var trimmedPath = path.replace(/^\/+|\/+$/g,'');
+
+    //Get the query string as an object
+    var queryStringObject = parsedUrl.query;
+
+    //Get the HTTP Method
+    var method = req.method.toUpperCase();
+
+    //Get the headers as an Object
+    var headers = req.headers;
+
+    //Send the response
+    res.end('Hello World\n');
+
+    //Log the request path 
+    console.log('Request is received on path: '+trimmedPath+'\nwith method '+method+ '\nand with these query parameters ',queryStringObject);
+    console.log('Request received with these headers ', headers);
+    
+});
+
+
+
+//Start the server, and have it listen on port 3000 
+server.listen(3000,function(){
+    console.log("The server is listening to port 3000 now");
+});
